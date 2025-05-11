ca5112ea3989f33b5d7454ca5c17971ccc0af47d
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-12 02:57:44 +0700

    Create tailwind.config.js

---
+++ application/views/site/tailwind.config.js
@@ -0,0 +1,4 @@
+!-- toggle dark mode -->
+<button onclick="document.documentElement.classList.toggle('dark')" class="ml-2 text-sm text-gray-600 dark:text-gray-300 hover:underline">
+  🌙 โหมดกลางคืน
+</button>
